1ddec399687b5cbdbac1670689f910aa9ffc8626
therealyo <dpshenik2002@gmail.com>
2022-07-28 15:54:36 +0300

    prettified

--- ip_geoposition/app.js
+++ ip_geoposition/app.js
@@ -1,21 +1,17 @@
-const express = require('express');
-const ipUtils = require("./ip_utils")
+const express = require("express");
+const ipUtils = require("./ip_utils");
 const app = express();
 
-
 const PORT = 3000;
-app.set('trust proxy', true);
-
-
+app.set("trust proxy", true);
 
 app.get("/", async (req, res) => {
     const response = await ipUtils.createResponse(req.ip);
     console.log(response);
 
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.send(response);
-})
-
+});
 
 app.listen(PORT, (err) => {
     if (err) console.log(err);
--- ip_geoposition/utils/ip.js
+++ ip_geoposition/utils/ip.js
@@ -1,88 +1,78 @@
-const fs = require('fs');
+const fs = require("fs");
 
-
-const GEOPOSITIONS = "IP2LOCATION-LITE-DB1.CSV";
+const GEOPOSITIONS = "../data/locations.csv";
 const IP_CONSTS = [16777216, 65536, 256, 1];
 
-
 const dotProduct = (arr1, arr2) => {
-    const product = arr1.map((x, i) => arr1[i] * arr2[i]); 
-    return product.reduce((a, b) => {return a + b;});
-    
-}
-
+    const product = arr1.map((x, i) => arr1[i] * arr2[i]);
+    return product.reduce((a, b) => {
+        return a + b;
+    });
+};
 
 const removeQuotes = (strArray) => {
-    return strArray.map(el => el.replaceAll('"', ''));
-}
-
+    return strArray.map((el) => el.replaceAll('"', ""));
+};
 
 const formatString = (str) => {
     return removeQuotes(str.split(","));
-}
-
+};
 
 const getInterval = (ipString) => {
     return [parseInt(ipString[0]), parseInt(ipString[1])];
-}
-
+};
 
 const convertToNumber = (ip) => {
     const values = ip.split(".");
     return dotProduct(values, IP_CONSTS);
-}
-
+};
 
 const readGeopositions = async (path) => {
     return (await fs.promises.readFile(path, "utf-8"))
-        .split('\n')
-        .map(str => formatString(str));
-}
-
+        .split("\n")
+        .map((str) => formatString(str));
+};
 
 const inInterval = (ipNumber, interval) => {
-    return (ipNumber >= interval[0] && ipNumber <= interval[1]);
-}
-
+    return ipNumber >= interval[0] && ipNumber <= interval[1];
+};
 
 const search = (ipNumber, ipsIntervals) => {
     let start = 0,
-          end = ipsIntervals.length - 1;
+        end = ipsIntervals.length - 1;
 
     while (start <= end) {
         const mid = Math.floor((start + end) / 2);
         const interval = getInterval(ipsIntervals[mid]);
-        
+
         if (inInterval(ipNumber, interval)) return mid;
         else if (ipNumber > interval[1]) start = mid + 1;
         else end = mid - 1;
     }
     return false;
-}
-
+};
 
 const findGeoposition = async (ip) => {
     const ips = await readGeopositions(GEOPOSITIONS);
     const ipNumber = convertToNumber(ip);
     const found = search(ipNumber, ips);
     return ips[found];
-}
+};
 
 const createResponse = async (ip) => {
     const geopostion = await findGeoposition(ip);
 
     const responseJSON = {
-        "ip": ip,
-        "ipDecimal": convertToNumber(ip),
-        "interval": getInterval(geopostion),
-        "countryCode": geopostion[2],
-        "country": geopostion[3].replaceAll("\r", "")
+        ip: ip,
+        ipDecimal: convertToNumber(ip),
+        interval: getInterval(geopostion),
+        countryCode: geopostion[2],
+        country: geopostion[3].replaceAll("\r", ""),
     };
 
     return JSON.stringify(responseJSON, null, 3);
-}
-
+};
 
 module.exports = {
-    createResponse
-}
+    createResponse,
+};
