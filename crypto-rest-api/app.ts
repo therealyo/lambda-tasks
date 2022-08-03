bbc4ec7d954417030b5e8b76ebf5905993363c49
therealyo <dpshenik2002@gmail.com>
2022-08-03 12:54:03 +0300

    add: configured package.json, tsconfig, eslintrc

--- crypto-rest-api/app.ts
+++ crypto-rest-api/app.ts
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3000;
 
 const app: Express = express();
 
+app.get('/:crypto', (req: Request, res: Response) => {
+    console.log(req.params.crypto);
+})
+
+app.get('/:platform', (req: Request, res: Response) => {
+    console.log(req.params.platform);
+})
+
+app.get('/:platform:crypto', (req: Request, res: Response) => {
+    console.log(req.params.platform);
+    console.log(req.params.crypto);
+})
+
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`)
 })
--- crypto-rest-api/dist/app.js
+++ crypto-rest-api/dist/app.js
@@ -6,6 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
+app.get('/:crypto', (req, res) => {
+    console.log(req.params.crypto);
+});
+app.get('/:platform', (req, res) => {
+    console.log(req.params.platform);
+});
+app.get('/:platform:crypto', (req, res) => {
+    console.log(req.params.platform);
+    console.log(req.params.crypto);
+});
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
 });
