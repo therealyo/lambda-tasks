5a8262950949e9b58d1678aa93ea65076ce8e0ce
therealyo <dpshenik2002@gmail.com>
2022-08-13 17:52:17 +0300

    fix: wrong time represantation

--- crypto-rest-api/lib/utils/date.ts
+++ crypto-rest-api/lib/utils/date.ts
@@ -1,6 +1,9 @@
 export const formatDate = (date: Date) => {
     return (
-        `${date.getFullYear()}-0${date.getMonth() + 1}-0${date.getDate()} ` +
+        `${date.getFullYear()}-` +
+        `0${date.getMonth() + 1}`.slice(-2) +
+        '-' +
+        `0${date.getDate()}`.slice(-2) +
         `0${date.getHours()}`.slice(-2) +
         `:` +
         `0${date.getMinutes()}`.slice(-2) +
